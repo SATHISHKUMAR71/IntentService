f2e5ff3e3811a9a76e039490d0a8d52781c3a63f
sathishkumar <satish102719@gmail.com>
2024-07-24 17:36:40 +0530

    Update the Functionality

--- app/src/main/java/com/example/intentservice/AppIntentService.kt
+++ app/src/main/java/com/example/intentservice/AppIntentService.kt
@@ -5,24 +5,34 @@ import android.content.Intent
 
 class AppIntentService:IntentService("intent") {
 
+    private var isRunning = false
     @Deprecated("Deprecated in Java")
     override fun onHandleIntent(intent: Intent?) {
 
         if(intent!=null){
             when(intent.action){
                 "ACTION_DOWNLOAD" ->{
-                    println("Download Started")
-                    Thread.sleep(2000)
-                    println("Download Finished")
+                    isRunning = true
+                    while (isRunning){
+                        Thread.sleep(1000)
+                        println("Data Downloading Started")
+                    }
+                    println("Data Downloaded Successfully")
                 }
                 "ACTION_UPLOAD" ->{
-                    println("Uploading Data")
-                    Thread.sleep(2000)
+                    isRunning = true
+                    for (i in 1..1000000){
+                        Thread.sleep(10)
+                        println("Data Uploading Started $i")
+                    }
                     println("Data Uploaded Successfully")
                 }
                 "ACTION_SYNC_DATA" -> {
-                    println("Data Sync Started")
-                    Thread.sleep(2000)
+                    isRunning = true
+                    for (i in 1..1000000){
+                        Thread.sleep(10)
+                        println("Data Sync Started")
+                    }
                     println("Data Sync Finished")
                 }
                 else->{
